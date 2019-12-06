bd35a04433e517997303c3e857012c2bcd7e5c10
Regino P <regino5298@gmail.com>
2019-12-06 11:23:38 -0700

    added styling and tests passed, met MVP

--- client/src/components/nav.js
+++ client/src/components/nav.js
@@ -1,5 +1,26 @@
 import React from 'react';
-import { useLightMode } from './hooks/useLightMode'
+import { useLightMode } from './hooks/useLightMode';
+import styled from 'styled-components';
+
+const Lbutton = styled.button `
+color: silver;
+box-sizing: border-box;
+background-color: transparent;
+border-radius: 0.6em;
+cursor: pointer;
+display: flex;
+align-self: center;
+font-size: 1rem;
+font-weight: 400;
+line-height: 1;
+margin: 20px;
+padding: 1.2em 2.8em;
+text-decoration: none;
+text-align: center;
+text-transform: uppercase;
+box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px #0000003d;
+font-weight: 700;
+`;
 
 const Navbar = () => {
   const [lightMode, setLightMode] = useLightMode(false);
@@ -7,7 +28,7 @@ const Navbar = () => {
     <nav>
       <h1>Women's World Cup 2019 Player Rankings</h1>
       <div>
-        <button onClick={e => {
+        <Lbutton onClick={e => {
           e.preventDefault();
           setLightMode(!lightMode);
           if (!lightMode) {
@@ -20,7 +41,7 @@ const Navbar = () => {
           className={lightMode ? "toggle toggled" : "toggle"}
         >
           Light Mode
-          </button>
+          </Lbutton>
       </div>
     </nav>
   );
--- client/src/components/playerCard.js
+++ client/src/components/playerCard.js
@@ -1,19 +1,38 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div `
+    display: grid;
+    grid-gap: 10rem;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: 1fr 1fr 1fr;
+    grid-template-areas: ". . ." ". . ." ". . .";
+`
+
+const CardWrapper = styled.div `
+    text-align: center;
+    background-color: azure;
+    width: 20vw;
+    margin-top: 5%;
+    padding: 5%;
+    border-radius: 20px;
+    box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px #0000003d;
+`;
 
 function PlayerCard(props) {
   console.log('PlayerCard: Props: ', props);
 
   return (
-    <div>
+    <Wrapper>
       {
         props.players.map(player => (
-          <div key={player.id}>
+          <CardWrapper key={player.id}>
             <h1>{player.name}</h1>
             <p>{player.country}</p>
-          </div>
+          </CardWrapper>
         ))
       }
-    </div>
+    </Wrapper>
   )
 }
 
